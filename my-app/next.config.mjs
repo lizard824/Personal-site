9bb115714e3ba741bcdacdab8cda746807f0e742
Kyle.Shen <kyle@Kyles-MacBook-Pro-2.local>
2024-05-31 11:52:20 +1000

    chore: Add build dir

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,16 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: shenkai03
+ * @Date: 2024-05-31 10:39:06
+ * @LastEditors: shenkai03
+ * @LastEditTime: 2024-05-31 11:49:49
+ * @FilePath: /my-app/next.config.mjs
+ * Copyright (C) 2024 shenkai03. All rights reserved.
+ */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+};
 
 export default nextConfig;
